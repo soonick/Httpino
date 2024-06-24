898dbb00aa5cc1cc613f8467b8b81a952da2264c
Adrian Ancona Novelo <soonick5@yahoo.com.mx>
2024-06-24 18:15:54 +0200

    parseQueryString returns a Hashtable copy instead of a pointer

--- test/src/httpino-test.cpp
+++ test/src/httpino-test.cpp
@@ -8,35 +8,31 @@ TEST_CASE("parseQueryString") {
   HttpIno http;
 
   SECTION("Single key value") {
-    Hashtable<String, String>* actual = http.parseQueryString("hello?abc=1");
-    REQUIRE(actual->elements() == 1);
-    REQUIRE(*actual->get("abc") == "1");
-    delete actual;
+    Hashtable<String, String> actual = http.parseQueryString("hello?abc=1");
+    REQUIRE(actual.elements() == 1);
+    REQUIRE(*actual.get("abc") == "1");
   }
 
   SECTION("Multiple key values") {
-    Hashtable<String, String>* actual =
+    Hashtable<String, String> actual =
         http.parseQueryString("hello?abc=1&qwer=world&onemore=yesyes");
-    REQUIRE(actual->elements() == 3);
-    REQUIRE(*actual->get("abc") == "1");
-    REQUIRE(*actual->get("qwer") == "world");
-    REQUIRE(*actual->get("onemore") == "yesyes");
-    delete actual;
+    REQUIRE(actual.elements() == 3);
+    REQUIRE(*actual.get("abc") == "1");
+    REQUIRE(*actual.get("qwer") == "world");
+    REQUIRE(*actual.get("onemore") == "yesyes");
   }
 
   SECTION("Handles spaces") {
-    Hashtable<String, String>* actual =
+    Hashtable<String, String> actual =
         http.parseQueryString("hello?full+name=Jose+Shanchez");
-    REQUIRE(actual->elements() == 1);
-    REQUIRE(*actual->get("full name") == "Jose Shanchez");
-    delete actual;
+    REQUIRE(actual.elements() == 1);
+    REQUIRE(*actual.get("full name") == "Jose Shanchez");
   }
 
   SECTION("Skips when no equal sign is found") {
-    Hashtable<String, String>* actual =
+    Hashtable<String, String> actual =
         http.parseQueryString("hello?asdf&abc=1");
-    REQUIRE(actual->elements() == 1);
-    REQUIRE(*actual->get("abc") == "1");
-    delete actual;
+    REQUIRE(actual.elements() == 1);
+    REQUIRE(*actual.get("abc") == "1");
   }
 }
